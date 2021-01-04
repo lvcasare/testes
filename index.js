6f91d3adc34823afa559a28b1b4f3420ee51f2e2
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-01-03 22:30:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var express = require('express');
 const app = express();
 
 app.get('/',(request,response)=>{
-return response.json({message:'Server is up'})
+return response.json({message:'Server is up2'})
 
 })
 
 app.listen(3333);
+console.log('Server is up');
